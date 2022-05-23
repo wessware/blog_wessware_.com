074071c570dbf49f38a283613c606bb6c32054b5
wessware <stevenshisia@yahoo.com>
2022-05-23 09:24:50 +0300

    topbar update

--- src/topbar/TopBar.jsx
+++ src/topbar/TopBar.jsx
@@ -3,7 +3,25 @@ import './topbar.css'
 export default function TopBar() {
   return (
       <div className='top'>
-         TopBar
+         <div className='topLeft'>
+             <i className='topIcon fab fa-facebook-square'></i>
+             <i className='topIcon fab fa-twitter-square'></i>
+             <i className='topIcon fab fa-reddit-square'></i>
+             <i className='topIcon fab fa-instagram-square'></i>
+         </div>
+         <div className='topCenter'>
+             <ul className='topList'>
+                 <li className='topListItem'>Home</li>
+                 <li className='topListItem'>Portfolio</li>
+                 <li className='topListItem'>Contact</li>
+                 <li className='topListItem'>Write</li>
+                 <li className='topListItem'>Log Out</li>
+             </ul>
+         </div>
+         <div className='topRight'>
+             <img className='topImg' src='' alt=''/>
+             <i className='topSearchIcon fas fa-search'></i>
+         </div>
     </div>
   )
 }
