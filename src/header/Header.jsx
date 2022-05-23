cecda57168b641114a277056ebdc015d069c34d5
wessware <stevenshisia@yahoo.com>
2022-05-23 09:48:13 +0300

    pages setup, header update

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,9 +1,14 @@
 import './header.css'
+import Image_Two from '../images/img-9.jpg'
 
 export default function Header() {
   return (
     <div className='header'>
-        Header
+        <div className='headerTitles'>
+            <span className='headerTitlesSm'>Life & Large</span>
+            <span className='headerTitleLg'>Blog</span>
+        </div>
+        <img className='headerImg' src={Image_Two} alt='header_image'/>
     </div>
   )
 }
