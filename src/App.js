12579b25941bc2cff7dca961e272b2105451f248
wessware <stevenshisia@yahoo.com>
2022-05-24 11:18:02 +0300

    app update

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import Home from "./pages/home/Home";
 import TopBar from "./components/topbar/TopBar";
 // eslint-disable-next-line
 import Single from "./pages/single/Single";
+// eslint-disable-next-line
 import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
 
 function App() {
   return (
     <>
      <TopBar />
-      <Write />
+      {/*<Write /> */}
+      <Settings />
     
     
     </>
