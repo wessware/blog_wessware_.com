f13748d83338450eb7effa3bdceeb59365d7c4c9
wessware <stevenshisia@yahoo.com>
2022-05-24 10:33:53 +0300

    single post jsx

---
+++ src/pages/single/Single.jsx
@@ -0,0 +1,12 @@
+import './Single.css'
+import Sidebar from '../../components/sidebar/Sidebar'
+import SinglePost from '../../components/singlePost/SinglePost'
+
+export default function Single() {
+    return (
+        <div className='single'>
+            <SinglePost />
+            <Sidebar/>
+        </div>
+    )
+}
