5c0927f81122b18fd7fc69ed6671220dfb82d386
wessware <stevenshisia@yahoo.com>
2022-05-24 10:30:49 +0300

    restructure

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,36 @@
+import './sidebar.css'
+import Img_three from '../../images/chastity.jpg'
+
+const Sidebar = () => {
+    return (
+        <div className='sidebar'>
+            <div className='sidebarItem'>
+                <span className='sidebarTitle'>ABOUT WESSWARE</span>
+                <img src={Img_three} alt='about_img'/>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus deleniti molestiae omnis placeat. Quos dolorum unde quam ut optio nemo distinctio pariatur, similique nisi nulla explicabo vel culpa sunt nam!</p>
+            </div>
+            <div className='sidebarItem'>
+                <span className='sidebarTitle'>CATEGORIES</span>
+                <ul className='sidebarList'>
+                    <li className='sidebarListItem'>TECH</li>
+                    <li className='sidebarListItem'>ART</li>
+                    <li className='sidebarListItem'>CAREERS</li>
+                    <li className='sidebarListItem'>POETRY</li>
+                </ul>
+            </div>
+            <div className='sidebarItem'>
+                <span className='sidebarTitle'>FOLLOW US</span>
+                <div className='sidebarSocial'>
+                        <i className="sidebarIcon fab fa-facebook-square"></i>
+                        <i className="sidebarIcon fab fa-twitter-square"></i>
+                        <i className="sidebarIcon fab fa-instagram-square"></i>
+                        <i className="sidebarIcon fab fa-linkedin"></i>
+                        <i className="sidebarIcon fab fa-github-square"></i>
+                </div>
+            </div>
+        </div>
+        
+    )
+}
+
+export default Sidebar
---
+++ src/components/topbar/TopBar.jsx
@@ -0,0 +1,29 @@
+import './topbar.css'
+import Img_One from '../../images/profile_img.jpg'
+
+
+export default function TopBar() {
+  return (
+      <div className='top'>
+         <div className='topLeft'>
+             <i className='topIcon fab fa-facebook-square'></i>
+             <i className='topIcon fab fa-twitter-square'></i>
+             <i className='topIcon fab fa-reddit-square'></i>
+             <i className='topIcon fab fa-instagram-square'></i>
+         </div>
+         <div className='topCenter'>
+             <ul className='topList'>
+                 <li className='topListItem'>Home</li>
+                 <li className='topListItem'>Portfolio</li>
+                 <li className='topListItem'>Contact</li>
+                 <li className='topListItem'>Write</li>
+                 <li className='topListItem'>Log Out</li>
+             </ul>
+         </div>
+         <div className='topRight'>
+         <img className='topImg' src={Img_One} alt='wess_image'/>
+             <i className='topSearchIcon fas fa-search'></i>
+         </div>
+    </div>
+  )
+}
