57ad5fe2ca739a7393a12e14ef81f32ebd167e9d
wessware <stevenshisia@yahoo.com>
2022-05-24 08:45:34 +0300

    post update

---
+++ src/post/Post.jsx
@@ -0,0 +1,8 @@
+import './post.css'
+export default function Post() {
+  return (
+    <div className='post'>
+        
+    </div>
+  )
+}
--- src/posts/Posts.jsx
+++ src/posts/Posts.jsx
@@ -1,9 +1,18 @@
 import './posts.css'
+import Post from '../post/Post'
 
 export default function Posts() {
   return (
     <div className='posts'>
-    Posts
+    <Post />
+    <Post />
+    <Post />
+    <Post />
+    <Post />
+    <Post />
+    <Post />
+    <Post />
+
     </div>
   )
 }
